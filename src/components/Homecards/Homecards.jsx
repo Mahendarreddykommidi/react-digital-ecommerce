47763592befe885f95c10a842ea722d18cc96a34
Mahenda reddy <mahendarreddykommidi82@gmail.com>
2025-04-14 22:39:54 +0530

    changes in homecards

--- src/components/Homecards/Homecards.jsx
+++ src/components/Homecards/Homecards.jsx
@@ -9,24 +9,24 @@ const Homecards = () => {
   return (
     <>  
 <div className="cards-section flex my-15  px-[10%] mx-auto ">
-  <div className="cards grid grid-cols-1 md:grid-cols-3 gap-10 items-center justify-items-center ">
+  <div className="cards grid grid-cols-1 md:grid-cols-3 gap-10 items-center justify-center ">
  
-    <div className="card  flex  items-center justify-center text-center space-x-5 ">
-    <img src={laptop} alt="" className="w-20" />
+    <div className="card  flex  items-center justify-center text-center space-x-5 shadow-2xl p-3 rounded-lg bg-white">
+    <img src={laptop} alt="" className="w-50" />
   
-      <div class="extra-text flex flex-col space-y-4">
+      <div class="extra-text flex flex-col space-y-5">
 
-            <p className="text-blue-500">new year sale</p>
-            <h3 className="font-bold text-2xl  flex flex-col">get and extra 50% off</h3>
+            <p className="text-blue-500 ">new year sale</p>
+            <h3 className="  flex flex-col">get and extra 50% off</h3>
             <a href="#">Shop now</a>
            
           </div>
     </div>
-    <div className="card flex  items-center justify-center text-center space-x-5 ">
+    <div className="card flex  items-center justify-center text-center space-x-5 shadow-2xl p-3 rounded-lg bg-white  ">
     <img src={iphone} alt="" className="w-50" />
       
       <div class="extra-text flex flex-col gap-2   ">
-            <p className="text-blue-400 font-light text-3xl capitalize">new year sale</p>
+            <p className="text-blue-400 ">new year sale</p>
             <h3>get and extra 50% off</h3>
             <a href="#">Shop now</a>
           
@@ -34,7 +34,7 @@ const Homecards = () => {
       
     
     </div>
-    <div className="card flex  items-center justify-center text-center space-x-5">
+    <div className="card flex  items-center justify-center text-center space-x-5 shadow-2xl p-3 rounded-lg bg-white">
     <img src={headphone} alt="" className="w-50" />
       <div class="extra-text flex flex-col gap-2">
             <p className="text-blue-500">new year sale</p>
